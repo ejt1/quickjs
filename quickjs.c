d23026d20d5b51d99f8f6221bb50ee1c4949feac
ejt <git@wector.info>
2023-06-02 11:57:00 +0200

    fix: always enable manual RNDNA rounding

--- quickjs.c
+++ quickjs.c
@@ -64,10 +64,9 @@
 #define MALLOC_OVERHEAD  8
 #endif
 
-#if !defined(_WIN32)
+/* XXX: Always enabled because needed even on windows */
 /* define it if printf uses the RNDN rounding mode instead of RNDNA */
 #define CONFIG_PRINTF_RNDN
-#endif
 
 /* define to include Atomics.* operations which depend on the OS
    threads */
